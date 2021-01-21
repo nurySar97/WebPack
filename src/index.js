4be47062432b3652457e4e8f2c3918978da26b76
Muhammet <muhammet@affexgroup.com>
2021-01-21 12:08:11 +0200

    XML && CSV - loaders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import Post from './Post';
 import './styles/style.css';
 import json from './assets/json';
-import Webpack from './assets/webpack-logo.png'
+import Webpack from './assets/webpack-logo.png';
+import xml from './assets/data.xml';
+import csv from './assets/data.csv'
 
 const post = new Post('Webpack Post Title', Webpack);
 
 console.log('Post To String: ', post.toString(), " This is index.js");
 
 console.log('JSON: ', json);
+console.log('XML: ', xml);
+console.log('CSV: ', csv)
--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,14 @@ module.exports = {
             {
                 test: /\.(ttf|woff|woff2|eot)$/,
                 use: ['file-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
             }
         ]
     }
